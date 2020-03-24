5d4b9be2da3ebfd170887aca6da603329114b27c
Emad A. Ismael <EmadAIsmael@yahoo.com>
2020-03-24 23:47:00 +0200

    Simple Chatty Bot Project finished files.

---
+++ Problems/Number squared/src/Task.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+// write your code here
+fun square(a: Int): Int = a * a
+
+/* Do not change code below */
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val number = scanner.nextInt()
+    println(square(number))
+}
